4f47cb1d8e8b3f8774ccf3769dafeabbec1d9aed
NicoleLund <78176837+NicoleLund@users.noreply.github.com>
2021-06-07 18:04:21 -0700

    two options for adding paths

--- run_all_ETL.py
+++ run_all_ETL.py
@@ -19,39 +19,47 @@ import pandas as pd
 
 
 #######################################################################
-# Add all subfolders to search path
+# Add all subfolders to search path - Option 1
 #######################################################################
 
-# Root Directory
-workspace_path = pathlib.Path().absolute()
-# print(f"workspace path: {workspace_path}")
+# # Root Directory
+# workspace_path = pathlib.Path().absolute()
 
-# Subfolder Directories
-holdings_cleanup_path = workspace_path.joinpath('1_holdings_cleanup')
-print(f"Holdings Cleanup Path: {holdings_cleanup_path}")
-sp500_scraping_path = workspace_path.joinpath('2_sp500_scraping')
-database_diagram_path = workspace_path.joinpath('3_database_diagram')
-sql_load_path = workspace_path.joinpath('4_sql_load')
-sql_analysis_path = workspace_path.joinpath('5_sql_analysis')
+# # Subfolder Directories
+# holdings_cleanup_path = workspace_path.joinpath('1_holdings_cleanup')
+# sp500_scraping_path = workspace_path.joinpath('2_sp500_scraping')
+# database_diagram_path = workspace_path.joinpath('3_database_diagram')
+# sql_load_path = workspace_path.joinpath('4_sql_load')
+# sql_analysis_path = workspace_path.joinpath('5_sql_analysis')
 
+# # Append subfolder directories
+# sys.path.append(holdings_cleanup_path)
+# sys.path.append(sp500_scraping_path)
+# sys.path.append(database_diagram_path)
+# sys.path.append(sql_load_path)
+# sys.path.append(sql_analysis_path)
+
+
+#######################################################################
+# Add all subfolders to search path - Option 2
+#######################################################################
 # Append subfolder directories
-sys.path.append(holdings_cleanup_path)
-sys.path.append(sp500_scraping_path)
-sys.path.append(database_diagram_path)
-sys.path.append(sql_load_path)
-sys.path.append(sql_analysis_path)
-print(f"System Path: {sys.path}")
+sys.path.append('1_holdings_cleanup')
+sys.path.append('2_sp500_scraping')
+sys.path.append('3_database_diagram')
+sys.path.append('4_sql_load')
+sys.path.append('5_sql_analysis')
 
 
-#######################################################################
+######################################################################
 # Import Transformed Holdings Data
-#######################################################################
-# from holdings_clean import df_final as holdings_df
+######################################################################
+from holdings_clean import df_final as holdings_df
 
-# # Review holdings data transferred
-# print('')
-# print('----- Holdings DataFrame Tranfer Verification -----')
-# print(holdings_df.head())
+# Review holdings data transferred
+print('')
+print('----- Holdings DataFrame Tranfer Verification -----')
+print(holdings_df.head())
 
 
 #######################################################################
