63b235c257bfa749ba2ef439316fd6fe0836b640
NicoleLund <78176837+NicoleLund@users.noreply.github.com>
2021-06-09 19:45:07 -0700

    final cleanup (#13)

---
+++ f_sql_analysis/postgres_pswd.py
@@ -0,0 +1,20 @@
+#######################################################################
+'''
+postgres_pswd.py
+----
+
+Written in the Python 3.7.9 Environment
+
+By Nicole Lund
+
+This Python script is a placeholder for the database server created by
+the user.  The user replaces the dummy text with their database details.
+'''
+#######################################################################
+ 
+
+# postgres database credentials
+host = "YOUR DATABASE HOST HERE"
+database = "YOUR DATABASE NAME HERE"
+username = "YOUR USERNAME HERE"
+passwd = "YOUR PASSWORD HERE"
